7e02e60a86d47c27383b0eb5e2f706bad5bce6a4
ayushrana03 <pundeerayush@gmail.com>
2025-12-05 15:53:24 +0530

    Add files via upload

---
+++ day24/q1.c
@@ -0,0 +1,18 @@
+//Write a program to print the following pattern:
+*
+**
+***
+****
+*****
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {       // rows
+        for (int j = 1; j <= i; j++) {   // columns
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
